3fcd60e440cdbc75a13d4fdd5b19a202ad2e3e36
CaioHPT <caio.h.teixeira@gmail.com>
2022-05-06 22:38:27 -0300

    ajuste da porta

--- src/server.ts
+++ src/server.ts
@@ -11,4 +11,4 @@ app.use(cors({
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => console.log(`Server is running...`))
+app.listen(process.env.PORT || 3333, () => console.log(`Server is running...`))
